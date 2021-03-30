d64fa48449ad8ae609bb58b66147de6d0f0a7e07
i9t6 <frquiroz@hotmail.com>
2021-03-29 20:08:32 -0600

    Create snmpPyTemp5_main.py

---
+++ snmpPyTemp5_main.py
@@ -0,0 +1,32 @@
+# -*- mode: python; python-indent: 4 -*-
+import ncs
+from ncs.application import Service
+
+# ------------------------
+# SERVICE CALLBACK EXAMPLE
+# ------------------------
+class ServiceCallbacks(Service):
+
+    @Service.create
+    def cb_create(self, tctx, root, service, proplist):
+        self.log.info('Service create(service=', service._path, ')')
+
+        snmpVars = ncs.template.Variables() 
+        snmpVars.add('COMMUNITY', "python-demo" + service.comm_str) 
+        snmpVars.add('ACCESS', service.access) 
+
+        template = ncs.template.Template(service)
+        template.apply('snmpPyTemp5-template', snmpVars)
+        self.log.info('snmpPyTemp5: comm: ', service.comm_str, ' access: ', service.access)
+
+# ---------------------------------------------
+# COMPONENT THREAD THAT WILL BE STARTED BY NCS.
+# ---------------------------------------------
+class Main(ncs.application.Application):
+    def setup(self):
+        self.log.info('Main RUNNING')
+        self.register_service('snmpPyTemp5-servicepoint', ServiceCallbacks)
+
+
+    def teardown(self):
+        self.log.info('Main FINISHED')
